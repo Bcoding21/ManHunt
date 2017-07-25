de8cec8a8c369c9fb27aa233a33d3dcc459cc15c
Ositanachi Otugo <ootugo@ootugo-macbookpro2.roam.corp.google.com>
2017-07-24 20:39:25 -0700

    Log In

--- app/src/main/java/com/brandon/manhunt/SignIn.java
+++ app/src/main/java/com/brandon/manhunt/SignIn.java
@@ -1,33 +1,38 @@
 package com.brandon.manhunt;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignIn extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    EditText mName;
-    EditText mPasswordField;
+    EditText mEmail, mPasswordField;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mName = (EditText)findViewById();
-        mPasswordField = (EditText)findViewById();
+        mEmail = (EditText)findViewById(R.id.email);
+        mPasswordField = (EditText)findViewById(R.id.password);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
     }
 
-    private void signIn(String email, String password){
+    private void signIn(final String email, final String password){
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -39,7 +44,7 @@ public class SignIn extends AppCompatActivity {
                         // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
                             Log.w(TAG, "signInWithEmail:failed", task.getException());
-                            Toast.makeText(EmailPasswordActivity.this, R.string.auth_failed,
+                            Toast.makeText(SignIn.this, R.string.auth_failed,
                                     Toast.LENGTH_SHORT).show();
                         }
 
