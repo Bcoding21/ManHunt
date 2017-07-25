ed1821d4ecf4cdb479aba3ebf4aafb8823895cf0
Brandon Cole <brandoncole@brandoncole-macbookpro.roam.corp.google.com>
2017-07-24 19:57:02 -0700

    Brandon

--- app/src/main/java/com/brandon/manhunt/SignUp.java
+++ app/src/main/java/com/brandon/manhunt/SignUp.java
@@ -6,7 +6,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -14,7 +18,7 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    EditText edit;
+    EditText mEmailField, mPasswordField, mDisplayName;
 
 
     @Override
@@ -28,9 +32,36 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         // Button
         findViewById(R.id.SignInButton).setOnClickListener(this);
 
+        // EditText
+        mEmailField = (EditText)findViewById(R.id.email);
+        mDisplayName = (EditText)findViewById(R.id.display);
+        mPasswordField = (EditText)findViewById(R.id.password);
+
     }
 
     public void onClick(View v){
+
+        createAcc(mDisplayName.getText().toString(),
+                mEmailField.getText().toString(),
+                mPasswordField.getText().toString());
+
+    }
+
+    private void createAcc(String email, String password, String displayName){
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignUp.this, "Sign up Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
 
 
     }
