55414a8bc99f28be292d1febc85af1b9675c7c84
Ositanachi Otugo <ootugo@ootugo-macbookpro2.roam.corp.google.com>
2017-07-24 19:55:56 -0700

    Sign-In Implementation
    Layout for sign in implementation

--- app/src/main/java/com/brandon/manhunt/SignIn.java
+++ app/src/main/java/com/brandon/manhunt/SignIn.java
@@ -6,15 +6,45 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignIn extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    EditText mName;
+    EditText mPasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mName = (EditText)findViewById();
+        mPasswordField = (EditText)findViewById();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+    }
+
+    private void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 
 }
