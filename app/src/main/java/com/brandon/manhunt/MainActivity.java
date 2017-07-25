435d039e1bd673679a0d5d2e96d5318aabce9b12
Brandon Cole <brandoncole@brandoncole-macbookpro.roam.corp.google.com>
2017-07-24 18:11:26 -0700

    Start over

--- app/src/main/java/com/brandon/manhunt/MainActivity.java
+++ app/src/main/java/com/brandon/manhunt/MainActivity.java
@@ -6,30 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
 
-    Button mSignUpButton, mSignInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSignInButton = (Button)findViewById(R.id.sign_in);
-        mSignUpButton = (Button)findViewById(R.id.sign_up);
-        mSignInButton.setOnClickListener(this);
-        mSignUpButton.setOnClickListener(this);
-    }
-
-    public void onClick(View v){
-        switch(v.getId()){
-            case R.id.sign_in:
-                startActivity(new Intent(this, Sign_in_page.class));
-                break;
-
-            case R.id.sign_up:
-                startActivity(new Intent(this,Sign_up_page.class));
-                break;
-
-        }
     }
 }
