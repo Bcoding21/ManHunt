9b4f2e60afdac635e7b0b3771fe3843f4170d314
Brandon Cole <brandoncole@brandoncole-macbookpro.roam.corp.google.com>
2017-07-26 16:28:00 -0700

    Progress dialog

--- app/src/main/java/com/brandon/manhunt/SignUp.java
+++ app/src/main/java/com/brandon/manhunt/SignUp.java
@@ -1,5 +1,6 @@
 package com.brandon.manhunt;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -25,6 +26,7 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     EditText mEmailField, mPasswordField, mDisplayName;
+    private ProgressDialog progress;
 
 
     @Override
@@ -71,13 +73,23 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
             Toast.makeText(this, "Please fill each field", Toast.LENGTH_SHORT).show();
         }
         else {
+            progress.setMessage("Working..");
+            progress.show();
+
             mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (!task.isSuccessful()) {
+                                progress.cancel();
                                 FirebaseAuthException e = (FirebaseAuthException)task.getException();
                                 Toast.makeText(SignUp.this, "Failed Registration: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                progress.cancel();
+                                Toast.makeText(SignUp.this, "Sign up complete", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignUp.this, MainPage.class));
+                                finish();
                             }
                         }
                     });
