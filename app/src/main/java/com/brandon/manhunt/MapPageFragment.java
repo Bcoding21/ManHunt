97770f2e1348d070c98336f1d400f639e10e1012
Lauren Clayton <claytonl@claytonl-macbookpro.roam.corp.google.com>
2017-08-02 23:28:09 -0700

    added map

--- app/src/main/java/com/brandon/manhunt/MapPageFragment.java
+++ app/src/main/java/com/brandon/manhunt/MapPageFragment.java
@@ -7,20 +7,61 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by brandoncole on 8/1/17.
  */
 
-public class MapPageFragment extends Fragment {
+public class MapPageFragment extends Fragment implements OnMapReadyCallback {
+    MapView mapView;
+    GoogleMap Gmap;
+    View mView;
 
 
-    private static final String TAG = "FRAGEMENT_MAP_PAGE";
-    @Nullable
+    public MapPageFragment(){}
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_map_page, container, false);
-
-
-        return v;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
-}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Gets the MapView from the XML layout and creates it
+
+
+        mView = inflater.inflate(R.layout.fragment_map_page, container, false);
+
+        return mView;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView = (MapView) mView.findViewById(R.id.Map);
+        if (mapView != null){
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        Gmap = googleMap;
+        Gmap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.609247, -74.044502)));
+        CameraPosition Liberty = CameraPosition.builder().target(new LatLng(40.609247, -74.044502)).zoom(16).bearing(0).build();
+    }
+    }
