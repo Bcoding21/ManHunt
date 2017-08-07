be5c721eb4f82a0e654bde50925fbe1cb72468e0
Brandon Cole <brandoncole@brandoncole-macbookpro.roam.corp.google.com>
2017-08-07 13:52:48 -0700

    Send/Receive location

--- app/src/main/java/com/brandon/manhunt/GamePageFragment.java
+++ app/src/main/java/com/brandon/manhunt/GamePageFragment.java
@@ -2,6 +2,8 @@ package com.brandon.manhunt;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,9 +19,13 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by brandoncole on 8/1/17.
@@ -31,7 +38,10 @@ public class GamePageFragment extends Fragment {
     private DatabaseReference mReference;
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
-    private String mCurrentUserEmail, mCurrentUserName;
+    private String mCurrentUserEmail, mCurrentUserName, mHuntedEmail;
+    private final int ONE_MINUTE = 60000, THIRTY_SECONDS = 30000;
+    public static final Handler handle = new Handler();
+    public static Runnable r;
 
 
     @Nullable
@@ -56,6 +66,7 @@ public class GamePageFragment extends Fragment {
         String unformattedEmail = mUser.getEmail();
         mCurrentUserEmail = unformattedEmail.replace("@", "at").replace(".", "dot");
 
+
         // set username
         Bundle b = getArguments();
         mCurrentUserName = b.getString("name");
@@ -63,7 +74,6 @@ public class GamePageFragment extends Fragment {
         // First time game setup
         if (savedInstanceState == null){
             addUser();  // add user as hunted or hunter to firebase
-            displayInfo(); // display if user is hunted/hunter to screen
         }
         else{
             String displayInfo = savedInstanceState.getString("display");
@@ -71,7 +81,6 @@ public class GamePageFragment extends Fragment {
         }
         return v;
     }
-
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
@@ -89,15 +98,26 @@ public class GamePageFragment extends Fragment {
         query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+
                 if (!dataSnapshot.exists()) {
 
                     User newUser = new User(mCurrentUserName, 0.0, 0.0);
                     mReference.child("Hunted").child(mCurrentUserEmail).setValue(newUser);
+                    mDisplayField.setText("You are being hunted");
+                    mReference.child("HuntedEmail").setValue(mCurrentUserEmail);
+                    sendHuntedLocation();
+
 
                 } else if (dataSnapshot.exists()) {
 
                     User newUser = new User(mCurrentUserName, 0.0, 0.0);
                     mReference.child("Hunters").child(mCurrentUserEmail).setValue(newUser);
+                    Map<String, ?> myMap = (HashMap)dataSnapshot.getValue();
+                    for (String key : myMap.keySet()){
+                        mHuntedEmail= key;
+                    }
+                    receiveHuntedLocation();
+
                 }
             }
             @Override
@@ -107,28 +127,21 @@ public class GamePageFragment extends Fragment {
         });
     }
 
+    private void   receiveHuntedLocation(){
+        DatabaseReference query = mReference.child("Hunted").child(mHuntedEmail);
 
-    private void displayInfo() {
-
-        DatabaseReference query = mReference.child("Hunted");
-
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
+        query.addValueEventListener(new ValueEventListener() {
+            double lat = 0;
+            double Longit = 0;
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                lat = (double)dataSnapshot.child("lat").getValue();
+                Longit = (double)dataSnapshot.child("long").getValue();
 
-                if (!dataSnapshot.exists()){
-                    mDisplayField.setText("You are being hunted!");
-                }
-                else{
-                    String huntedName = null;
-
-                    for (DataSnapshot data : dataSnapshot.getChildren()){ // only one child
-                        huntedName = data.child("displayName").getValue().toString();
-                    }
-
-                    mDisplayField.append(huntedName);
-                }
+                mClosestHunters.setText("LONG: " + Longit + "\n LAT: " + lat);
             }
+
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
@@ -136,9 +149,26 @@ public class GamePageFragment extends Fragment {
         });
     }
 
+    private void sendHuntedLocation() {
+
+             final DatabaseReference query = mReference.child("Hunted");
+             r = new Runnable() {
+                 double lat = 0.1;
+                double Long = 0.1;
+
+                public void run() {
+                    query.child(mCurrentUserEmail).child("lat").setValue(++lat);
+                    query.child(mCurrentUserEmail).child("long").setValue(++Long);
+                    handle.postDelayed(this, 1000);
+                }
+            };
+            handle.postDelayed(r, 0);
+    }
 
 
-
-
-
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        handle.removeCallbacks(r);
+    }
 }
