911c281a550848028cac40413d5d1f9c5d4723e1
Brandon Cole <brandoncole@brandoncole-macbookpro.roam.corp.google.com>
2017-08-10 14:15:56 -0700

    Can end game and exit

--- app/src/main/java/com/brandon/manhunt/gamePage.java
+++ app/src/main/java/com/brandon/manhunt/gamePage.java
@@ -2,11 +2,14 @@ package com.brandon.manhunt;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
+import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -55,7 +58,7 @@ public class gamePage extends AppCompatActivity {
     private LocationManager locationManager;
     private long THIRTY_SECONDS = 30000;
     private double mLongitude, mLatitude;
-    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationListener mLocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,7 +77,6 @@ public class gamePage extends AppCompatActivity {
 
         // set Up location
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         //Firebase
         mReference = FirebaseDatabase.getInstance().getReference();
@@ -86,18 +88,22 @@ public class gamePage extends AppCompatActivity {
     }
 
     private void startSession(){
+
         mReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (!dataSnapshot.hasChild("Hunted")){  // if user is being hunted
 
+                    mReference.child("GAMEOVER").setValue(false);
                     User user = new User(mCurrentUserEmail, 0, 0);
                     mReference.child("Hunted").setValue(user);
                     passInfoToGameFragment(true);
+                    mHuntedEmail = mCurrentUserEmail;
+
+
                 }
 
                 else if (dataSnapshot.hasChild("Hunted")){ // if user is a hunter
-
 
                     User user = new User(mCurrentUserEmail, 0, 0);
                     mReference.child("Hunters").child(mCurrentUserEmail).setValue(user);
@@ -119,6 +125,7 @@ public class gamePage extends AppCompatActivity {
             sendLocation(isHunted);
             String s = "YOU ARE BEING HUNTED!";
             GamePageFragment.getInstance().getInformation(s);
+            listenForGameOver();
 
         }
         else{
@@ -133,46 +140,51 @@ public class gamePage extends AppCompatActivity {
             checkLocationPermission();
         }
 
+
+
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                double latitude = location.getLatitude();
+                //get the latitude
+                double longitude = location.getLongitude();
+                //get the longitude
+
+                String name1 = mCurrentUserEmail;
+                String name2 = mHuntedEmail;
+
+                if (isHunted){
+                    mReference.child("Hunted").child("lat").setValue(latitude);
+                    mReference.child("Hunted").child("long").setValue(longitude);
+                    getHuntersLocation(latitude, longitude);
+                }
+                else{
+                    mReference.child("Hunters").child(mCurrentUserEmail).child("lat").setValue(latitude);
+                    mReference.child("Hunters").child(mCurrentUserEmail).child("long").setValue(longitude);
+                }
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
         if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 7000, 0, new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    double latitude = location.getLatitude();
-                    //get the latitude
-                    double longitude = location.getLongitude();
-                    //get the longitude
-
-                    String name1 = mCurrentUserEmail;
-                    String name2 = mHuntedEmail;
-
-                    if (isHunted){
-                        mReference.child("Hunted").child("lat").setValue(latitude);
-                        mReference.child("Hunted").child("long").setValue(longitude);
-                        getHuntersLocation(latitude, longitude);
-                    }
-                    else{
-                        mReference.child("Hunters").child(mCurrentUserEmail).child("lat").setValue(latitude);
-                        mReference.child("Hunters").child(mCurrentUserEmail).child("long").setValue(longitude);
-                    }
-
-                }
-
-                @Override
-                public void onStatusChanged(String s, int i, Bundle bundle) {
-
-                }
-
-                @Override
-                public void onProviderEnabled(String s) {
-
-                }
-
-                @Override
-                public void onProviderDisabled(String s) {
-
-                }
-            });
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 7300, 0, mLocationListener);
         }
     }
 
@@ -182,15 +194,21 @@ public class gamePage extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
-                mLatitude = dataSnapshot.child("lat").getValue(Double.class);
-                mLongitude = dataSnapshot.child("long").getValue(Double.class);
+                if (!dataSnapshot.exists()){
+                    GamePageFragment.getInstance().getInformation("HUNTED HAS LEFT. WAITING FOR ANOTHER...");
+                }
+                else {
+                    mLatitude = dataSnapshot.child("lat").getValue(Double.class);
+                    mLongitude = dataSnapshot.child("long").getValue(Double.class);
 
-                Location location = new Location("");
-                location.setLatitude(mLatitude);
-                location.setLongitude(mLongitude);
+                    Location location = new Location("");
+                    location.setLatitude(mLatitude);
+                    location.setLongitude(mLongitude);
 
-                GamePageFragment.getInstance().recieveLocation(location);
-                MapPageFragment.getInstance().updateMap(mLatitude, mLongitude);
+                    GamePageFragment.getInstance().getInformation("You are hunted: " + mHuntedUsername);
+                    GamePageFragment.getInstance().recieveLocation(location);
+                    MapPageFragment.getInstance().updateMap(mLatitude, mLongitude);
+                }
             }
 
             @Override
@@ -264,6 +282,7 @@ public class gamePage extends AppCompatActivity {
 
                 String s = "You are hunting: " + mHuntedUsername;
                 GamePageFragment.getInstance().getInformation(s);
+                listenForGameOver();
             }
 
 
@@ -308,5 +327,67 @@ public class gamePage extends AppCompatActivity {
         }
     }
 
+    private void listenForGameOver(){
 
+        mReference.child("GAMEOVER").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue(Boolean.class)){
+                    endGame();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void endGame(){
+
+
+        locationManager.removeUpdates(mLocationListener);
+
+        if (mCurrentUserEmail.equals(mHuntedEmail)) { // if hunted
+            GamePageFragment.getInstance().getInformation("YOU HAVE BEEN CAUGHT \n GAME OVER!");
+            mReference.child("Hunted").setValue(null);
+        }
+
+        else{
+            GamePageFragment.getInstance().getInformation("THE HUNTED HAS BEEN CAUGHT \n GAME OVER!");
+            mReference.child("Hunters").child(mCurrentUserEmail).setValue(null);
+        }
+
+
+        new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long l) {
+                GamePageFragment.getInstance().setSecondDisplay("Leaving session in: " + l / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(gamePage.this, MainPage.class));
+            }
+        }.start();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(this, "YOU HAVE LEFT THE GAME", Toast.LENGTH_SHORT).show();
+
+        locationManager.removeUpdates(mLocationListener);
+        locationManager = null;
+
+        if (mCurrentUserEmail.equals(mHuntedEmail)){
+            mReference.child("Hunted").setValue(null);
+        }
+        else{
+            mReference.child("Hunters").child(mCurrentUserEmail).setValue(null);
+        }
+    }
 }
