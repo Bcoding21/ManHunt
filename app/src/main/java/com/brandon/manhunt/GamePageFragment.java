974a825c6c39bc23254402611e8ac35cf76edab7
Gideon Driver <gdriver@gdriver-macbookpro.roam.corp.google.com>
2017-08-15 21:17:52 -0700

    dunno

--- app/src/main/java/com/brandon/manhunt/GamePageFragment.java
+++ app/src/main/java/com/brandon/manhunt/GamePageFragment.java
@@ -1,27 +1,17 @@
 package com.brandon.manhunt;
 
-import android.location.Location;
-import android.media.MediaPlayer;
-
-import java.util.List;
-
 import android.content.Intent;
 import android.location.Location;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationServices;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
